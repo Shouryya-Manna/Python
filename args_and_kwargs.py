eb47a17903dbbfb692998dd9182f24f2458d5dda
Shouryya <shouryyamanna06@gmail.com>
2025-01-31 11:51:09 +0530

    first commit

---
+++ args_and_kwargs.py
@@ -0,0 +1,12 @@
+def shipping_label(*args,**kwargs):
+    for arg in args:
+        print(arg,end=" ")
+    print()
+    for key,value in kwargs.items():
+        print(f"{key}:{value}")
+
+
+shipping_label("Mr.","Shouryya","Manna",
+               item="Rifle",
+               type="Sniper",
+               name="Dragnoauv")
